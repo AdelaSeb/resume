01b2c9fa6c07880ccc3bf0150ed7a9de991192ec
AdelaSeb <a.sebestova@outlook.com>
2020-04-05 17:43:03 -0700

    removed react GA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
@@ -19,8 +18,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
+    
 
   }
 
